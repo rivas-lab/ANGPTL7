c522bf0a2597f24316f298a2250aa43c2d8443ef
Yosuke Tanigawa <ytanigaw@stanford.edu>
2019-06-11 19:14:46 -0700

    update

---
+++ notebook/ukbb_summary_stats_analysis/GWAS_IOP/gwas_quick_check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -beEuo pipefail
+
+#cat /oak/stanford/groups/mrivas/ukbb24983/phenotypedata/10136/21731/phe/INI5255.phe | awk '($3 == -9){print $1}' | sort -b | join -1 1 -2 1 /dev/stdin <(cat /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe | cut -f1 | sort -b) | awk '{print $0, $0}' > ukb24983_white_british_no_IOP.phe
+
+
+var_id=$1
+
+var_id='rs28991009'
+
+echo $var_id \
+| plink2 \
+  --bpfile /oak/stanford/groups/mrivas/private_data/ukbb/24983/cal/pgen/ukb24983_cal_cALL_v2_hg19 \
+  --chr 1-22 \
+  --covar /oak/stanford/groups/mrivas/ukbb24983/sqc/ukb24983_GWAS_covar.phe \
+  --covar-name age sex Array PC1-PC4 \
+  --glm firth-fallback hide-covar omit-ref \
+  --memory 25600 \
+  --pheno /oak/stanford/groups/mrivas/ukbb24983/phenotypedata/extras/highconfidenceqc/phe/HC276.phe \
+  --pheno-quantile-normalize \
+  --remove /oak/stanford/groups/mrivas/ukbb24983/sqc/ukb24983_v2.not_used_in_pca.phe \
+  --threads 4 \
+  --keep ukb24983_white_british_no_IOP.phe \
+  --extract /dev/stdin \
+  --out ukb24983_v2_hg19.INI5255.genotyped.${var_id}
+
+#  --extract /oak/stanford/groups/mrivas/ukbb24983/sqc/both_array_variants.txt \
+#  --out /oak/stanford/groups/mrivas/ukbb24983/cal/gwas/extras/highconfidenceqc/white_british/ukb24983_v2_hg19.HC276.genotyped.both_arrays
+#  --keep /oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_white_british.phe \
+
