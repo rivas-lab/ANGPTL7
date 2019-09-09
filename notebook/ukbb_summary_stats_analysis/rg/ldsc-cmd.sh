e6519e11f523e5cdf29b8c98324748d5dd159ca6
Yosuke Tanigawa <ytanigaw@stanford.edu>
2019-09-08 17:25:45 -0700

    add results of LDSC

---
+++ notebook/ukbb_summary_stats_analysis/rg/ldsc-cmd.sh
@@ -0,0 +1,1 @@
+bash ~/repos/rivas-lab/ukbb-tools/07_LDSC/scripts/ukb_ldsc_munge_rg.sh data/ukb24983_v2_hg19.INI5255.genotyped.PHENO1.glm.linear.gz data/ukb24983_v2_hg19.INI5254.genotyped.PHENO1.glm.linear.gz ANGPTL7
