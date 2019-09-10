b22cd2f35431709316a9e41ee228b060f9f0403f
Yosuke Tanigawa <yk.tanigawa@gmail.com>
2019-09-10 15:01:37 +0300

    add rg and pop stratification plots

--- notebook/ukbb_summary_stats_analysis/rg/ldsc-cmd.sh
+++ notebook/ukbb_summary_stats_analysis/rg/ldsc-cmd.sh
@@ -1,1 +1,9 @@
-bash ~/repos/rivas-lab/ukbb-tools/07_LDSC/scripts/ukb_ldsc_munge_rg.sh data/ukb24983_v2_hg19.INI5255.genotyped.PHENO1.glm.linear.gz data/ukb24983_v2_hg19.INI5254.genotyped.PHENO1.glm.linear.gz ANGPTL7
+#!/bin/bash
+set -beEuo pipefail
+
+for GBE_ID in INI5254 INI5262 INI5263 ; do
+    bash ~/repos/rivas-lab/ukbb-tools/07_LDSC/scripts/ukb_ldsc_munge_rg.sh \
+    data/ukb24983_v2_hg19.INI5255.genotyped.PHENO1.glm.linear.gz \
+    data/ukb24983_v2_hg19.${GBE_ID}.genotyped.PHENO1.glm.linear.gz \
+    ANGPTL7
+done
